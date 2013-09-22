0dee2385c7b6134db2adb0cf57447341822b5aef
guiem <g@guiem.info>
2013-09-22 12:37:05 +0200

    twenty_thirteen_post_nav function rewritten to call adjacent posts from same category, still not working

--- functions.php
+++ functions.php
@@ -5,8 +5,39 @@
  * Declare textdomain for this child theme.
  * Translations can be filed in the /languages/ directory.
  */
-function my_child_theme_setup() {
+function twentythirteenchild_setup() {
     load_child_theme_textdomain( 'twentythirteenchild', get_stylesheet_directory() . '/languages' );
 }
-add_action( 'after_setup_theme', 'my_child_theme_setup' );
+add_action( 'after_setup_theme', 'twentythirteenchild_setup' );
+    
+    
+/**
+ * Displays navigation to next/previous post when applicable.
+ *
+ * @since Twenty Thirteen 1.0
+ *
+ * @return void
+ */
+function twentythirteen_post_nav() {
+    global $post;
+    echo 'entro';
+    // Don't print empty markup if there's nowhere to navigate.
+    $previous = ( is_attachment() ) ? get_post( $post->post_parent ) : get_adjacent_post( true, '', true );
+    $next     = get_adjacent_post( true , '', false );
+    
+    if ( ! $next && ! $previous )
+        return;
+    ?>
+    <nav class="navigation post-navigation" role="navigation">
+    <h1 class="screen-reader-text"><?php _e( 'Post navigation', 'twentythirteen' ); ?></h1>
+    <div class="nav-links">
+
+    <?php previous_post_link( '%link', _x( '<span class="meta-nav">&larr;</span> %title', 'Previous post link', 'twentythirteen' ) ); ?>
+    <?php next_post_link( '%link', _x( '%title <span class="meta-nav">&rarr;</span>', 'Next post link', 'twentythirteen' ) ); ?>
+
+    </div><!-- .nav-links -->
+    </nav><!-- .navigation -->
+    <?php
+}
+
 ?>
